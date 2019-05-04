1eb0c38bb8d2a302b949dc893f4860c5fddec5c9
Shawn Chapla <shwnchpl@gmail.com>
2019-05-03 22:30:15 -0400

    Make dev configuration an explicit test config
    Moves development configuration into __main__.py and makes it an
    explicit test config.

--- abrv/__init__.py
+++ abrv/__init__.py
@@ -8,11 +8,6 @@ from . import url
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        # TODO: Move DB config to config.py.
-        DATABASE={'database': 'abrv', 'user': 'moth', 'password': '',}
-    )
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
--- abrv/__main__.py
+++ abrv/__main__.py
@@ -1,5 +1,13 @@
 from . import create_app
+from getpass import getuser
 
 if __name__ == '__main__':
-    app = create_app()
+    app = create_app(test_config={
+        'SECRET_KEY': 'dev',
+        'DATABASE': {
+            'database': 'abrv',
+            'user': getuser(),
+            'password': '',
+        }
+    })
     app.run()
